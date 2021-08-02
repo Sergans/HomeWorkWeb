2a54e6207b593adf08bb0a9b49fc754ae3cf14be
Сергей <gan7er@yandex.ru>
2021-08-02 17:17:05 +0300

    the final version

--- TaskWeb-1/TaskWeb-1/Program.cs
+++ TaskWeb-1/TaskWeb-1/Program.cs
@@ -4,25 +4,30 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Collections.Generic;
 
 namespace TaskWeb_1
 {
     
     class Program
     {
+        static string path = @"C:\Users\GANS\Desktop\HomeWorkWeb\post.txt";
         private static readonly HttpClient client = new HttpClient();
         public static async Task GetAs(int id)
         {    string url = "http://jsonplaceholder.typicode.com/posts/";
              client.DefaultRequestHeaders.Accept.Clear();
              var stringTask = client.GetStringAsync($"{url}/{id}");
-             var msg = await stringTask;
-             Console.Write(msg);
+           await File.AppendAllTextAsync(path, await stringTask +Environment.NewLine);
+           
         }
 
         static async Task Main(string[] args)
         {
-          await GetAs(1);
-          await GetAs(2);
+            for (int i = 4; i <= 13; i++)
+            {
+                await GetAs(i);
+            }
+           
         }
     }
 }
